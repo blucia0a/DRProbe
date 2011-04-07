ac7372b5219c8d4f83f31aff52a141fb6a8594fb
Nick Hunt <nhunt@cs.washington.edu>
2011-04-07 15:43:11 -0700

    support for x86

--- debug_mod/debug_mod.c
+++ debug_mod/debug_mod.c
@@ -9,12 +9,45 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
-
-#define AUTHOR "Nicholas Hunt <nhunt@cs.washington.edu>"
-#define DESC   "Make debug registers accesible via the /proc filesystem"
+#include <linux/moduleparam.h>
 
 #define PROC_DIRNAME "dr"
-#define NR_DRS    8
+#define NR_DRS       8
+
+#define DBG_VERSION 0.8
+#define DBG_AUTHOR "Nicholas Hunt <nhunt@cs.washington.edu>"
+#define DBG_DESC   "Make debug registers accesible via the /proc filesystem"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DBG_AUTHOR);
+MODULE_DESCRIPTION(DBG_DESC);
+//MODULE_VERSION(DBG_VERSION);
+
+#ifdef CONFIG_X86_32
+	#define DR_SIZE 4
+	#define READ_DR(nr, ret) asm("movl %%dr" #nr ", %0" : "=r" (ret));
+	#define WRITE_DR(nr, val) \
+       		do { \
+			asm("movl %0, %%dr" #nr :: "r" (val)); \
+			current->thread.debugreg ##nr = val;  \
+		} while(0)
+
+#elif CONFIG_X86_64
+	#define DR_SIZE 8
+	#define READ_DR(nr, ret) asm("movq %%dr" #nr ", %0" : "=r" (ret));
+	#define WRITE_DR(nr, val) \
+       		do { \
+			asm("movq %0, %%dr" #nr :: "r" (val)); \
+			current->thread.debugreg ##nr = val;  \
+		} while(0)
+#else
+	#error "Only x86 and x86_64 architectures are supported\n"
+#endif
+
+
+static int debug = 0;
+module_param(debug, int, 0);
+//MODULE_PARAM_DESC(debug, "Set to a non-zero value to enable debug messages");
 
 struct proc_dir_entry *proc_dir = NULL;
 struct proc_dir_entry *drs[NR_DRS] = {NULL};
@@ -24,49 +57,61 @@ static void remove_proc_entries(void);
 static int debug_mod_read(char *page, char **start, off_t off, int count, int *eof, void *data);
 static int debug_mod_write(struct file *file, const char __user *buffer, unsigned long cnt, void *data);
 
+static void debug_print(const char *fmt, ...)
+{
+        va_list args;
+        int r;
+
+	if (debug) {
+		va_start(args, fmt);
+		r = vprintk(fmt, args);
+		va_end(args);
+	}
+}
+
 static int __init debug_mod_init(void)
 {
 	int ret = 0;
 
-	printk(KERN_INFO "debug_mod loading...\n");
+	debug_print(KERN_INFO "debug_mod loading...\n");
 
 	ret = create_proc_entries();
 	if (ret != 0) {
-		printk(KERN_INFO "Something bad happened.\n");
+		debug_print(KERN_INFO "Something bad happened.\n");
 		return ret;
 	}
 
-	printk(KERN_INFO "done.\n");
+	debug_print(KERN_INFO "done.\n");
 	return 0;
 }
 
 static void __exit debug_mod_exit(void)
 {
 	remove_proc_entries();
-	printk(KERN_INFO "debug_mod unloaded.\n");
+	debug_print(KERN_INFO "debug_mod unloaded.\n");
 }
 
 int create_proc_entries(void)
 {
-	int i, j, ret = 0;
+	long i, j, ret = 0;
 	char buffer[32];
 
-	printk(KERN_INFO "  Creating proc entries\n");
+	debug_print(KERN_INFO "  Creating proc entries\n");
 
 	proc_dir = proc_mkdir(PROC_DIRNAME, NULL);
 	if (proc_dir == NULL) {
 		ret = -1;
-		printk(KERN_INFO "Couldn't create dir /proc/%s\n", PROC_DIRNAME);
+		debug_print(KERN_INFO "Couldn't create dir /proc/%s\n", PROC_DIRNAME);
 		goto out_ret;
 	}
 
 	for (i = 0; i < NR_DRS; i++) {
-		snprintf(buffer, sizeof buffer, "dr%d", i);
-		printk(KERN_INFO "    Creating /proc/%s/%s...\n", PROC_DIRNAME, buffer);
+		snprintf(buffer, sizeof buffer, "dr%ld", i);
+		debug_print(KERN_INFO "    Creating /proc/%s/%s...\n", PROC_DIRNAME, buffer);
 
 		drs[i] = create_proc_entry(buffer, 0666, proc_dir);
 		if (drs[i] == NULL) {
-			printk(KERN_INFO "  Couldn't create proc entry\n");
+			debug_print(KERN_INFO "  Couldn't create proc entry\n");
 			ret = -1;
 			goto out_free_dir;
 		}
@@ -77,20 +122,20 @@ int create_proc_entries(void)
 		drs[i]->mode  = S_IFREG | S_IRUGO | S_IWUGO;
 		drs[i]->uid   = 0;
 		drs[i]->gid   = 0;
-		drs[i]->size  = 8;
-		drs[i]->data  = (void *)((uint64_t)i);
+		drs[i]->size  = DR_SIZE;
+		drs[i]->data  = (void *)(i);
 	}
 
 	return ret;
 
  out_free_dir:
  	for (j = 0; j < i; j++) {
-		snprintf(buffer, sizeof buffer, "dr%d", i);
-		printk(KERN_INFO "  Removing /proc/%s/%s\n", PROC_DIRNAME, buffer);
+		snprintf(buffer, sizeof buffer, "dr%ld", j);
+		debug_print(KERN_INFO "  Removing /proc/%s/%s\n", PROC_DIRNAME, buffer);
 		remove_proc_entry(buffer, proc_dir);
 	}
 
-	printk(KERN_INFO "  Removing /proc/%s\n", PROC_DIRNAME);
+	debug_print(KERN_INFO "  Removing /proc/%s\n", PROC_DIRNAME);
 	remove_proc_entry(PROC_DIRNAME, NULL);
 
  out_ret:
@@ -102,34 +147,32 @@ void remove_proc_entries(void)
 	int i;
 	char buffer[32];
 
-	printk(KERN_INFO "Removing proc entries\n");
+	debug_print(KERN_INFO "Removing proc entries\n");
 
 	for (i = 0; i < NR_DRS; i++) {
 		snprintf(buffer, sizeof buffer, "dr%d", i);
-		printk(KERN_INFO "    Removing /proc/%s/%s...\n", PROC_DIRNAME, buffer);
+		debug_print(KERN_INFO "    Removing /proc/%s/%s...\n", PROC_DIRNAME, buffer);
 		remove_proc_entry(buffer, proc_dir);
 	}
 
 	remove_proc_entry(PROC_DIRNAME, NULL);
-
-	printk(KERN_INFO "Done.\n");
+	debug_print(KERN_INFO "Done.\n");
 }
-
 
 static long read_dr(int nr)
 {
 	long ret = -EINVAL;
 	switch (nr) {
-		case 0: asm("movq %%dr0, %0" : "=r" (ret)); break;
-		case 1: asm("movq %%dr1, %0" : "=r" (ret)); break;
-		case 2: asm("movq %%dr2, %0" : "=r" (ret)); break;
-		case 3: asm("movq %%dr3, %0" : "=r" (ret)); break;
-		case 4: asm("movq %%dr4, %0" : "=r" (ret)); break;
-		case 5: asm("movq %%dr5, %0" : "=r" (ret)); break;
-		case 6: asm("movq %%dr6, %0" : "=r" (ret)); break;
-		case 7: asm("movq %%dr7, %0" : "=r" (ret)); break;
+		case 0: READ_DR(0, ret); break;
+		case 1: READ_DR(1, ret); break;
+		case 2: READ_DR(2, ret); break;
+		case 3: READ_DR(3, ret); break;
+		case 4: READ_DR(4, ret); break;
+		case 5: READ_DR(5, ret); break;
+		case 6: READ_DR(6, ret); break;
+		case 7: READ_DR(7, ret); break;
 		default:
-			printk(KERN_INFO "Invalid debug register number %d\n", nr);
+			debug_print(KERN_INFO "Invalid debug register number %d\n", nr);
 			break;
 	}
 
@@ -140,14 +183,14 @@ static long write_dr(int nr, long val)
 {
 	long ret = 0;
 	switch (nr) {
-		case 0: asm("movq %0, %%dr0" :: "r" (val)); current->thread.debugreg0 = val; break;
-		case 1: asm("movq %0, %%dr1" :: "r" (val)); current->thread.debugreg1 = val; break;
-		case 2: asm("movq %0, %%dr2" :: "r" (val)); current->thread.debugreg2 = val; break;
-		case 3: asm("movq %0, %%dr3" :: "r" (val)); current->thread.debugreg3 = val; break;
-		case 6: asm("movq %0, %%dr6" :: "r" (val)); current->thread.debugreg6 = val; break;
-		case 7: asm("movq %0, %%dr7" :: "r" (val)); current->thread.debugreg7 = val; break;
+		case 0: WRITE_DR(0, val); break;
+		case 1: WRITE_DR(1, val); break;
+		case 2: WRITE_DR(2, val); break;
+		case 3: WRITE_DR(3, val); break;
+		case 6: WRITE_DR(6, val); break;
+		case 7: WRITE_DR(7, val); break;
 		default:
-			printk(KERN_INFO "Invalid debug register number %d\n", nr);
+			debug_print(KERN_INFO "Invalid debug register number %d\n", nr);
 			ret = -EINVAL;
 			break;
 	}
@@ -161,10 +204,10 @@ static int debug_mod_read(char *page, char **start, off_t off,
 	int nr = (int)((long)data);
 	long val;
 
-	printk(KERN_INFO "Reading %d:dr%d\n", current->pid, nr);
+	debug_print(KERN_INFO "Reading %d:dr%d\n", current->pid, nr);
 	val = read_dr(nr);
 
-	printk(KERN_INFO "  Got value %ld (%lx)\n", val, val);
+	debug_print(KERN_INFO "  Got value %ld (%lx)\n", val, val);
 	*((long *)page) = val;
 	*eof = 1;
 
@@ -180,21 +223,21 @@ static int debug_mod_write(struct file *file, const char __user *buffer,
 	cnt = cnt > sizeof val ? sizeof val : cnt;
 
 	if (!access_ok(VERIFY_READ, buffer, cnt)) {
-		printk(KERN_INFO "Bad user address\n");
+		debug_print(KERN_INFO "Bad user address\n");
 		return -EPERM;
 	}
 
 	if (copy_from_user(&val, buffer, cnt) != 0) {
-		printk(KERN_INFO "Couldn't copy %ld bytes\n", cnt);
+		debug_print(KERN_INFO "Couldn't copy %ld bytes\n", cnt);
 		return -EACCES;
 	}
 
 	set_thread_flag(TIF_DEBUG);
 
-	printk(KERN_INFO "Writing %d:dr%d, val:%ld (%lx)\n", current->pid, nr, val, val);
+	debug_print(KERN_INFO "Writing %d:dr%d, val:%ld (%lx)\n", current->pid, nr, val, val);
 	ret = write_dr(nr, val);
 	val = read_dr(nr);
-	printk(KERN_INFO "  Reading to check: %ld (%lx)\n", val, val);
+	debug_print(KERN_INFO "  Reading to check: %ld (%lx)\n", val, val);
 
 	return cnt;
 }
@@ -202,6 +245,3 @@ static int debug_mod_write(struct file *file, const char __user *buffer,
 module_init(debug_mod_init);
 module_exit(debug_mod_exit);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR(AUTHOR);
-MODULE_DESCRIPTION(DESC);
--- debug_mod/debug_mod_test.c
+++ debug_mod/debug_mod_test.c
@@ -5,16 +5,27 @@
 #include <signal.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <ucontext.h>
 
-void sigtrap(int nr)
+void sigtrap_sa(int nr, siginfo_t *si, void *__ctx)
 {
-	printf("Trap! (%d)\n", nr);
+	int i;
+
+	long  val;
+	int fd = open("/proc/dr/dr6", O_RDONLY);
+	read(fd, &val, sizeof val);
+	close(fd);
+
+	if (val & 0x1) printf("Breakpoint 0\n");
+	if (val & 0x2) printf("Breakpoint 1\n");
+	if (val & 0x4) printf("Breakpoint 2\n");
+	if (val & 0x8) printf("Breakpoint 3\n");
 }
 
 int main(int argc, char *argv[])
 {
 	int dr;
-	uint64_t val;
+	long val;
 	int fd, n;
 	char line[64];
 	char buffer[32];
@@ -22,10 +33,14 @@ int main(int argc, char *argv[])
 	char *tok1, *tok2, *tok3;
 	char *tok;
 
-	signal(SIGTRAP, sigtrap);
+	struct sigaction sa;
+	sa.sa_sigaction = sigtrap_sa;
+	sa.sa_flags     = SA_SIGINFO;
+	sigaction(SIGTRAP, &sa, NULL);
 	
 	printf("&cmd: %p\n", &cmd);
-	printf("ll:   %d\n", sizeof(long long));
+	printf("&dr : %p\n", &dr);
+	printf("&val: %p\n", &val);
 
 	while (1) {
 		fd = -1, dr = -1;
@@ -75,7 +90,7 @@ int main(int argc, char *argv[])
 
 		case 'r':
 			n = read(fd, &val, sizeof val);
-			printf("Got %d bytes: %ld\n", n, val);
+			printf("Got %d bytes: %ld (%lx)\n", n, val, val);
 			break;
 		
 		default:
